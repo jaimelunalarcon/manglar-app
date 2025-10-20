21a237f5e5b247c64dbdb8da8b5cf0b5df737f94
jaime <jaimeluna.a@gmail.com>
2025-10-20 08:54:02 -0300

    tabla finanzas

--- src/pages/admin/Finanzas.jsx
+++ src/pages/admin/Finanzas.jsx
@@ -3,15 +3,15 @@ import Table from 'react-bootstrap/Table';
 
 function Finanzas() {
   return (
-  <div className='p-4 bg-white border mt-3'>
+  <div>
     <h2 className='mb-5'>Finanzas (Admin)</h2>
     <Table hover responsive>
         <thead>
-          <tr className='text-center bg-dark text-white'>
-            <th className=' bg-light'>Usuario</th>
-            <th className=' bg-light'>Puntos</th>
-            <th className=' bg-light'>Abono</th>
-            <th className=' bg-light'>Deuda</th>
+          <tr className='text-center text-white'>
+            <th className=' bg-dark text-white'>Usuario</th>
+            <th className=' bg-dark text-white'>Puntos</th>
+            <th className=' bg-dark text-white'>Abono</th>
+            <th className=' bg-dark text-white'>Deuda</th>
           </tr>
         </thead>
         <tbody>
@@ -36,6 +36,37 @@ function Finanzas() {
   
         </tbody>
       </Table>
+
+      <Table hover responsive className='mt-3 mb-3'>
+        <thead>
+          <tr className=' bg-dark text-white'>
+            <th className=' bg-dark text-white'>Gasto</th>
+            <th className='text-center bg-dark text-white'>Fecha</th>
+            <th className='text-center bg-dark text-white'>Valor</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Feria</td>
+            <td className='text-center'>01/10/25</td>
+            <td className='text-center'>$40.000</td>
+          </tr>
+          <tr>
+             <td>Productos de Aseo</td>
+            <td className='text-center'>04/10/25</td>
+            <td className='text-center'>$20.000</td>
+          </tr>
+          <tr>
+             <td>Arreglo Techo</td>
+            <td className='text-center'>10/10/2025</td>
+            <td className='text-center'>$200.000</td>
+          </tr>
+  
+        </tbody>
+      </Table>
+      <card className="border bg-white d-block p-3 rounded">
+        Total ingresos casa <span className='float-end'>$210.000</span>
+      </card>
   </div>
 )};
 
